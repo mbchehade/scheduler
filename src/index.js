9a9ee977ff72257251d3e2448a9170ddc91b0cc6
Mohamed <mbchehade@gmail.com>
2020-02-25 18:31:10 -0700

    implementing netlify

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import ReactDOM from "react-dom";
 import "index.scss";
 
 import Application from "components/Application";
+import axios from "axios"
 
 ReactDOM.render(<Application />, document.getElementById("root"));
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
