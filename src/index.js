f7299353fac9c1ab01e09db51e08d643210053d2
Mohamed <mbchehade@gmail.com>
2020-02-25 19:35:56 -0700

    added npm i -f

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import "index.scss";
 import Application from "components/Application";
 import axios from "axios"
 
-ReactDOM.render(<Application />, document.getElementById("root"));
-
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
+
+ReactDOM.render(<Application />, document.getElementById("root"));
